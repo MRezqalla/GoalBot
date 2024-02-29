692981d9a17461f372877c1b0ed6c170bc2b5fc0
MRezqalla <=>
2024-02-28 22:26:59 -0500

    setup ros2_control

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -8,7 +8,7 @@ def generate_launch_description():
 
         Node(
             package='rplidar_ros',
-            executable='rplidar_composition',
+            executable='rplidar_node',
             output='screen',
             parameters=[{
                 'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
