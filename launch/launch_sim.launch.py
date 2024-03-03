8f048f679e40de90e992db75ccaeac788c4da95a
mrezqalla <rezqallamohammad@gmail.com>
2024-03-02 21:12:28 -0500

    controller launch

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -25,6 +25,12 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true','use_ros2_control':'true'}.items()
     )
 
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
     gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config','gazebo_params.yaml')
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
@@ -40,15 +46,16 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
-    diff_drive_spawner = Node(package="controller_manager", executable="spawner.py",
+    diff_drive_spawner = Node(package="controller_manager", executable="spawner",
                               arguments=["diff_cont"],)
 
-    joint_broad_spawner = Node(package="controller_manager", executable="spawner.py",
+    joint_broad_spawner = Node(package="controller_manager", executable="spawner",
                               arguments=["joint_broad"],)
 
     # Launch them all!
     return LaunchDescription([
         rsp,
+        joystick,
         gazebo,
         spawn_entity,
         diff_drive_spawner,
