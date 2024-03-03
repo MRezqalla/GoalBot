3f51eb71fbcc5c7c750a2d40a7f2ed41fcc57e6b
mrezqalla <rezqallamohammad@gmail.com>
2024-03-03 13:41:40 -0500

    SLAM and nav2

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -13,10 +13,6 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
     package_name='goal_bot' 
 
     rsp = IncludeLaunchDescription(
@@ -29,6 +25,15 @@ def generate_launch_description():
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','joystick.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    twist_mux_params = os.path.join(get_package_share_directory(package_name), 'config','twist_mux.yaml')
+
+    twist_mux = Node(
+        package="twist_mux",
+        executable="twist_mux",
+        parameters=[twist_mux_params, {'use_sim_time':'true'}],
+        remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')],
     )
 
     gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config','gazebo_params.yaml')
@@ -62,6 +67,7 @@ def generate_launch_description():
     return LaunchDescription([
         rsp,
         joystick,
+        twist_mux,
         gazebo,
         spawn_entity,
         diff_drive_spawner,
