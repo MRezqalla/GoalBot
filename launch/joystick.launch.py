aad6ba1877a0e2fd9ff4d955eca8731544014913
mrezqalla <rezqallamohammad@gmail.com>
2024-03-23 14:23:04 -0400

    add

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -28,7 +28,7 @@ def generate_launch_description():
     # twist_stamper = Node(
     #     package='twist_stamper',
     #     executable='twist_stamper',
-        # parameters=[{'use_sim_time':use_sim_time}],
+    #     parameters=[{'use_sim_time':use_sim_time}],
     #     remappings=[
     #         ('/cmd_vel_in','/diff_cont/cmd_vel_unstamped'),
     #         ('/cmd_vel_out','/diff_cont/cmd_vel')
